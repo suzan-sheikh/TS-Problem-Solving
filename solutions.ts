e9ddb84dbea20177d9d2149eb7b47152e0407818
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-07 19:17:58 +0600

    7th Problem solved

--- solutions.ts
+++ solutions.ts
@@ -1,37 +1,35 @@
 // Problem_1:
-
 const filterEvenNumbers = (inNum: number[]): number[] =>
   inNum.filter((n) => n % 2 === 0);
 
-// Problem_2:
 
+// Problem_2:
 const reverseString = (inStr: string): string =>
   inStr.split("").reverse().join("");
 
-// Problem_3:
 
+// Problem_3:
 type StringOrNumber = string | number;
 const checkType = (iStrNum: StringOrNumber): string =>
   typeof iStrNum === "string" ? "String" : "Number";
 
-// Problem_4:
 
+// Problem_4:
 const getProperty = <T>(inputObj: T, inputKey: keyof T) => inputObj[inputKey];
 
-// Problem_5:
 
+// Problem_5:
 interface Book {
   title: string;
   author: string;
   publishedYear: number;
 }
-
 const toggleReadStatus = (inputBook: Book): Book & { isRead: boolean } => {
   return { ...inputBook, isRead: true };
 };
 
-// Problem_6:
 
+// Problem_6:
 class Person {
   name: string;
   age: number;
@@ -50,3 +48,8 @@ class Student extends Person {
     return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.age}`;
   }
 }
+
+
+// Problem_7:
+const getIntersection = (inArrOne: number[], inArrTwo: number[]): number[] =>
+  inArrOne.filter((i) => inArrTwo.includes(i));
